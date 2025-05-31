ef815b040f30988179cfe4b7d446fed9a01aa97d
juanmbarrientos <juanmbarrientos@users.noreply.github.com>
2025-05-31 01:12:13 +0000

    deploy: 43554f02e3c18f10776fd48f6499711b08fd33ec

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/widget/', // Reemplazá con el nombre de tu repositorio
-  plugins: [react()],
-})
